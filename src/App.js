6f5c155e860811cff8542c1bb422bb3fa6653474
fasbender <alamgirfaisalbin@gmail.com>
2021-03-11 13:26:39 +0600

    Slider Work in Progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ImageSlider from './components/ImageSlider/ImageSlider';
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <ImageSlider/>
     </div>
   );
 }
