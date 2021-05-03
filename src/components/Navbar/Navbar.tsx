61465b7db05ef5c6c6207f7c54e5627488d3b0a2
pervysage <zulkerb9b@gmail.com>
2021-05-04 02:16:55 +0600

    Dropdownn done responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,22 +5,20 @@ import { Dropdown, Transition, Menu } from "semantic-ui-react";
 import { RootStoreContext } from "../../stores/rootStore";
 import SvgComponent from "./logosvg";
 import { MenuItems } from "./MenuItems";
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from "react-responsive";
 import "./Navbar.css";
 
 const Navbar = () => {
-
   const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)'
-  })
-  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    query: "(min-device-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-device-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   const isTabletOrMobileDevice = useMediaQuery({
-    query: '(max-device-width: 1224px)'
-  })
-  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
-
+    query: "(max-device-width: 1224px)",
+  });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
   const rootStore = useContext(RootStoreContext);
   const { normalLogin, bookingLogin, setFeatured } = rootStore.navStore;
@@ -69,13 +67,13 @@ const Navbar = () => {
 
   return (
     <Menu pointing className="NavbarItems">
-      <Link to={"/"} className= "forlink">
+      <Link to={"/"} className="forlink">
         <SvgComponent />
       </Link>
       <div className="menu-icon" onClick={handleClick}>
         <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
-      <ul className={clicked ? "nav-menu active" : "nav-menu"} >
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
         {MenuItems.map((item, index) => {
           return (
             <li key={index} onClick={handleClick}>
@@ -87,7 +85,8 @@ const Navbar = () => {
                 onClick={
                   item.url === "/onlineBooking" && !user
                     ? bookingLogin
-                    : item.url === "/ourProject" ? setFeatured
+                    : item.url === "/ourProject"
+                    ? setFeatured
                     : () => {}
                 }
               >
@@ -96,10 +95,42 @@ const Navbar = () => {
             </li>
           );
         })}
-        {isTabletOrMobileDevice && <li onClick={handleClick}>
-          {user ? <Link to="">{user?.fullname}</Link> : <Link to="/login" >LOGIN</Link>}
-        </li>}
-        
+        {isTabletOrMobileDevice && (
+          <li>
+            {user ? (
+              <Dropdown className="dropname-mob " text={user.fullname}>
+              <Dropdown.Menu
+                className="dropmenu-mob"
+                style={{
+                  backgroundColor: "white",
+                  top: "50px",
+                  right: "0",
+                  left: "10px",
+                  width: "20px",
+                  fontWeight:900
+                }}
+              >
+                <Dropdown.Item
+                  text="My Allotments "
+                  onClick={handleAllotment}
+                />
+                <Dropdown.Item text="My Bookings" onClick={handleBookings} />
+                <Dropdown.Item
+                  text="Transfer Allotments"
+                  onClick={handleTransfer}
+                />
+                <Dropdown.Item
+                  text="Profile Settings"
+                  onClick={handleProfile}
+                />
+                <Dropdown.Item text="Logout" onClick={handleLogout} />
+              </Dropdown.Menu>
+            </Dropdown>
+            ) : (
+              <Link to="/login">LOGIN</Link>
+            )}
+          </li>
+        )}
       </ul>
       <div className="right-content">
         <h6>CONTACT US</h6>
@@ -109,45 +140,45 @@ const Navbar = () => {
         <h6>02 4881 1616</h6>
         <i className="navfont fas fa-cart-plus"></i>
       </div>
-      {!isTabletOrMobileDevice && <div className="login-content">
-        {user ? (
-          <div className="foruser">
-            <Dropdown className="dropname " text={user.fullname}>
-              <Dropdown.Menu
-                className="dropmenu"
-                style={{
-                  backgroundColor:"goldenrod",
-                  top: "50px",
-                  right: "0",
-                  left: "20px",
-                  width: "20px",
-                }}
-              >
-                <Dropdown.Item
-                  text="My Allotments "
-                  onClick={handleAllotment}
-                />
-                <Dropdown.Item
-                  text="My Bookings" 
-                  onClick={handleBookings}
-                />
-                <Dropdown.Item
-                  text="Transfer Allotments"
-                  onClick={handleTransfer}
-                />
-                <Dropdown.Item text="Profile Settings" 
-                onClick={handleProfile}/>
-                <Dropdown.Item text="Logout" onClick={handleLogout} />
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        ) : (
-          <Link to="/login">
-            <button onClick={normalLogin}>LOG IN</button>
-          </Link>
-        )}
-      </div>}
-      
+      {!isTabletOrMobileDevice && (
+        <div className="login-content">
+          {user ? (
+            <div className="foruser">
+              <Dropdown className="dropname " text={user.fullname}>
+                <Dropdown.Menu
+                  className="dropmenu"
+                  style={{
+                    backgroundColor: "goldenrod",
+                    top: "50px",
+                    right: "0",
+                    left: "20px",
+                    width: "20px",
+                  }}
+                >
+                  <Dropdown.Item
+                    text="My Allotments "
+                    onClick={handleAllotment}
+                  />
+                  <Dropdown.Item text="My Bookings" onClick={handleBookings} />
+                  <Dropdown.Item
+                    text="Transfer Allotments"
+                    onClick={handleTransfer}
+                  />
+                  <Dropdown.Item
+                    text="Profile Settings"
+                    onClick={handleProfile}
+                  />
+                  <Dropdown.Item text="Logout" onClick={handleLogout} />
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          ) : (
+            <Link to="/login">
+              <button onClick={normalLogin}>LOG IN</button>
+            </Link>
+          )}
+        </div>
+      )}
     </Menu>
   );
 };
