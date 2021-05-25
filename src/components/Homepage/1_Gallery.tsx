302af6ef9f40b7cb51cdd5b947b23e6a4657c6b5
pervysage <zulkerb9b@gmail.com>
2021-05-25 23:48:13 +0600

    UsedProgressiveImage for faster loading

--- src/components/Homepage/1_Gallery.tsx
+++ src/components/Homepage/1_Gallery.tsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./gallery.css";
+import useProgressiveImg from "../OurProject/UseProgressiveImg";
 
 const Gallery = () => {
   const settings = {
@@ -18,6 +19,10 @@ const Gallery = () => {
     pauseOnHover: false
 
   };
+  const [src0, { blur0 }] = useProgressiveImg("/images/h1.jpg","/h1.jpg");
+  const [src1, { blur1 }] = useProgressiveImg("/images/h4.jpg","/h4.jpg");
+  const [src2, { blur2 }] = useProgressiveImg("/images/o2.jpg","/o2.jpg");
+  const [src3, { blur3 }] = useProgressiveImg("/images/o3.jpg","/o3.jpg");
 
     return (
       <>
@@ -53,8 +58,13 @@ const Gallery = () => {
             </div>
             <img
               className="logoimg"
-              src={process.env.PUBLIC_URL + "/images/h1.jpg"}
+              src={src0}
+              // src={process.env.PUBLIC_URL + "/images/h1.jpg"}
               alt=""
+              style={{
+                filter: blur0 ? "blur(20px)" : "none",
+                transition: blur0 ? "none" : "filter 0.3s ease-out"
+              }}
             />
           </div>
           <div className="gallery_img ">
@@ -86,8 +96,13 @@ const Gallery = () => {
             </div>
             <img
               className="logoimg"
-              src={process.env.PUBLIC_URL + "/images/h4.jpg"}
+              src={src1}
+              // src={process.env.PUBLIC_URL + "/images/h1.jpg"}
               alt=""
+              style={{
+                filter: blur1 ? "blur(20px)" : "none",
+                transition: blur1 ? "none" : "filter 0.3s ease-out"
+              }}
             />
           </div>
           <div className="gallery_img ">
@@ -118,8 +133,13 @@ const Gallery = () => {
             </div>
             <img
               className="logoimg"
-              src={process.env.PUBLIC_URL + "/images/o2.jpg"}
+              src={src2}
+              // src={process.env.PUBLIC_URL + "/images/h1.jpg"}
               alt=""
+              style={{
+                filter: blur2 ? "blur(20px)" : "none",
+                transition: blur2 ? "none" : "filter 0.3s ease-out"
+              }}
             />
           </div>
           <div className="gallery_img ">
@@ -151,8 +171,13 @@ const Gallery = () => {
             </div>
             <img
               className="logoimg"
-              src={process.env.PUBLIC_URL + "/images/o3.jpg"}
+              src={src3}
+              // src={process.env.PUBLIC_URL + "/images/h1.jpg"}
               alt=""
+              style={{
+                filter: blur3 ? "blur(20px)" : "none",
+                transition: blur3 ? "none" : "filter 0.3s ease-out"
+              }}
             />
           </div>
         </Slider>
