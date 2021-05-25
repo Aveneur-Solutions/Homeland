efd2b2f78407446ae84210d88834789f09d5100d
pervysage <zulkerb9b@gmail.com>
2021-05-25 22:00:29 +0600

    fixed responsive issues.

--- src/components/OurProject/BuildingShowcase.tsx
+++ src/components/OurProject/BuildingShowcase.tsx
@@ -1,33 +1,68 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 import ListUnits from "./ListUnits";
+import { useMediaQuery } from "react-responsive";
 
 const BuildingShowcase = () => {
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
   return (
-    <div className="building3">
-      <div className="projCardContainer">
-        <Card fluid>
-          <Image
-            src={process.env.PUBLIC_URL + "/images/h2.jpg"}
-            fluid
-            ui={false}
-          />
-          <Card.Content>
-            <Card.Header>Building 1</Card.Header>
-            <Card.Meta>
-              <span className="date">Available 4</span>
-              <span className="date">Booked 4</span>
-            </Card.Meta>
-            <Card.Description>
-              This South facing house provides good breeze.
-            </Card.Description>
-          </Card.Content>
-        </Card>
-        <div className="unitscroll">
-          <ListUnits />
+    <>
+      {!isTabletOrMobileDevice ? (
+        <div className="building3">
+          <div className="projCardContainer">
+            <Card fluid>
+              <Image
+                src={process.env.PUBLIC_URL + "/images/h2.jpg"}
+                fluid
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>Building 1</Card.Header>
+                <Card.Meta>
+                  <span className="date">Available 4</span>
+                  <span className="date">Booked 4</span>
+                </Card.Meta>
+                <Card.Description>
+                  This South facing house provides good breeze.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <div className="unitscroll">
+              <ListUnits />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        
+        // MOBILE VIEW CODE STARTS FROM BELOW
+        <div className="building3m">
+          <div className="projCardContainerm">
+            <Card fluid>
+              <Image
+                src={process.env.PUBLIC_URL + "/images/h2.jpg"}
+                fluid
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>Building 1</Card.Header>
+                <Card.Meta>
+                  <span className="date">Available 4</span>
+                  <span className="date">Booked 4</span>
+                </Card.Meta>
+                <Card.Description>
+                  This South facing house provides good breeze.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <div className="unitscrollm">
+              <ListUnits />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/OurProject/ListUnits.tsx
+++ src/components/OurProject/ListUnits.tsx
@@ -1,39 +1,78 @@
 import { useEffect } from "react";
 import { Card, Button, Image, Grid } from "semantic-ui-react";
 import { ourProject } from "./buildings";
-const ListUnits = () => {
-  return (
-    <Grid columns={1}>
-          <div className="projCardSize">
+import { useMediaQuery } from "react-responsive";
 
-          {ourProject.map((buildno, index) => {
-            return (
-        <Card fluid className="projCardSize2" >
-              <div className="projCardSize2" >
-                <Card.Content>
-                  <Image src={process.env.PUBLIC_URL + buildno.forunits} />
-                  <Card.Header>Unit 1.00.1</Card.Header>
-                  <Card.Meta>Sqft.: 950</Card.Meta>
-                  <Card.Description>
-                    <strong>Price: 3,000,000</strong>
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <div className="ui two buttons">
-                    <Button basic color="green">
-                      View
-                    </Button>
-                    <Button basic color="red">
-                      Add to Cart
-                    </Button>
+const ListUnits = () => {
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
+  return (
+    <>
+      {!isTabletOrMobileDevice ? (
+        <Grid columns={1}>
+          <div className="projCardSize">
+            {ourProject.map((buildno, index) => {
+              return (
+                <Card fluid className="projCardSize2">
+                  <div className="projCardSize2">
+                    <Card.Content>
+                      <Image src={process.env.PUBLIC_URL + buildno.forunits} />
+                      <Card.Header>Unit 1.00.1</Card.Header>
+                      <Card.Meta>Sqft.: 950</Card.Meta>
+                      <Card.Description>
+                        <strong>Price: 3,000,000</strong>
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <div className="ui two buttons">
+                        <Button basic color="green">
+                          View
+                        </Button>
+                        <Button basic color="red">
+                          Add to Cart
+                        </Button>
+                      </div>
+                    </Card.Content>
                   </div>
-                </Card.Content>
-              </div>
-        </Card>
-            );
-          })}
-        </div>
-    </Grid>
+                </Card>
+              );
+            })}
+          </div>
+        </Grid>
+      ) : (
+        
+        // MOBILE CODE SECTION FROM BELOW
+        <div className="projCardSize">
+            {ourProject.map((buildno, index) => {
+              return (
+                <Card fluid className="projCardSize2">
+                  <div className="projCardSize2">
+                    <Card.Content>
+                      <Image src={process.env.PUBLIC_URL + buildno.forunits} />
+                      <Card.Header>Unit 1.00.1</Card.Header>
+                      <Card.Meta>Sqft.: 950</Card.Meta>
+                      <Card.Description>
+                        <strong>Price: 3,000,000</strong>
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <div className="ui two buttons">
+                        <Button basic color="green">
+                          View
+                        </Button>
+                        <Button basic color="red">
+                          Add to Cart
+                        </Button>
+                      </div>
+                    </Card.Content>
+                  </div>
+                </Card>
+              );
+            })}
+          </div>
+      )}
+    </>
   );
 };
 
