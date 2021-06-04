a5c309567b505221990c05ba299f9779b4d222b6
fasbender <alamgirfaisalbin@gmail.com>
2021-06-05 00:07:56 +0600

    updated transfer unit

--- src/components/Dropdown/TransferForm/TransferUnit.tsx
+++ src/components/Dropdown/TransferForm/TransferUnit.tsx
@@ -25,10 +25,12 @@ const TransferUnit = () => {
             <div className="online-booking-container">
             {confirmTransfer && <button className="btn" onClick={() => setConfirmTransfer(false)}>Back to transfer</button>}
                 {!confirmTransfer &&
-                    <div>
-                        <UserSearchForm getUser={searchUser} user={recieverUser!} />
-                        {recieverUser && <UserCard setUnitVisibility={setUnitVisibility} user={recieverUser} />}
-                        {unitVisibility && myBookedFlats && <MyUnits bookedUnits={transferrableFlats} setConfirmTransfer={setConfirmTransfer} />}
+                    <div className="transfer-form-container" style={{display: 'flex'}}>
+                        <div style={{width: '100%'}}><UserSearchForm getUser={searchUser} user={recieverUser!} /></div>
+                        <div className={recieverUser ? "border-true" : "border-false"} style={{padding: '5px'}}>
+                            {recieverUser && <UserCard setUnitVisibility={setUnitVisibility} user={recieverUser} />}
+                            {unitVisibility && myBookedFlats && <MyUnits bookedUnits={transferrableFlats} setConfirmTransfer={setConfirmTransfer} />}
+                        </div>
                     </div>
                 }
                 {confirmTransfer && recieverUser && <ConfirmTransfer  />}
