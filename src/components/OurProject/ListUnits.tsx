83d6aaa63000405556f11c555ae2f9ffbe2226da
Ashik80 <ashikurrahman020995@gmail.com>
2021-06-04 19:58:26 +0600

    One last time

--- src/components/OurProject/ListUnits.tsx
+++ src/components/OurProject/ListUnits.tsx
@@ -22,10 +22,10 @@ const ListUnits = () => {
                   <div className="projCardSize2">
                     <Card.Content>
                       <Image
-                        style={{
-                          filter: blur ? "blur(20px)" : "none",
-                          transition: blur ? "none" : "filter 0.3s ease-out",
-                        }}
+                        // style={{
+                        //   filter: blur ? "blur(20px)" : "none",
+                        //   transition: blur ? "none" : "filter 0.3s ease-out",
+                        // }}
                         // src={src}
                         src={process.env.PUBLIC_URL + "//d5twn4m6.cdn.imgeng.in/images/profile_main.png"}
                         // src={process.env.PUBLIC_URL + buildno.forunits}
@@ -61,7 +61,8 @@ const ListUnits = () => {
               <Card key={index} fluid className="projCardSize2">
                 <div className="projCardSize2">
                   <Card.Content>
-                    <Image src={src} />
+                    {/* <Image src={src} /> */}
+                    src={process.env.PUBLIC_URL + "//d5twn4m6.cdn.imgeng.in/images/profile_main.png"}
                     <Card.Header>Unit 1.00.1</Card.Header>
                     <Card.Meta>Sqft.: 950</Card.Meta>
                     <Card.Description>
